9efc1be8e55fb22f3d97ac1b21288807e7e5beb0
ugruenheid <uwe.gruenheid@1und1.de>
2018-12-12 07:57:21 +0100

    [update] + made it a little bit more realistic by really using bean

--- src/main/java/com/mixaaa/jackson/demo/configuration/SecondObjectMapperAutoConfiguration.java
+++ src/main/java/com/mixaaa/jackson/demo/configuration/SecondObjectMapperAutoConfiguration.java
@@ -1,17 +1,21 @@
 package com.mixaaa.jackson.demo.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @AutoConfigureAfter(JacksonAutoConfiguration.class)
+@ConditionalOnClass(Jackson2ObjectMapperBuilder.class)
 public class SecondObjectMapperAutoConfiguration {
 
 	@ConfigurationProperties(prefix = "logging.jackson")
@@ -21,8 +25,8 @@ public class SecondObjectMapperAutoConfiguration {
 	}
 
 	@Bean
-	public ObjectMapper secondObjectMapper() {
-		return loggingJacksonProperties().initializeJackson2ObjectMapperBuilder().build();
+	public ObjectMapper secondObjectMapper(@Qualifier("loggingJacksonProperties") JacksonProperties loggingJacksonProperties) {
+		return loggingJacksonProperties.initializeJackson2ObjectMapperBuilder().build();
 	}
 
 	@Bean
